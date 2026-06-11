3c06ea7985f446276713005ee3d9a6bfde9c5394
Andyana Nabila <113494333+chocoricano@users.noreply.github.com>
2026-06-11 16:31:36 +0700

    submit jawaban exercise

---
+++ exercise_andy/src/App.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+import model.Customer;
+import service.CustomerService;
+
+public class App {
+    public static void main(String[] args) {
+        CustomerService customerService = new CustomerService();
+        customerService.createCustomer(
+            "Budi Santoso", 
+            "budi@mail.com", 
+            "08123456789");
+        customerService.createCustomer(
+            "Siti Aminah", 
+            "siti@mail.com", 
+            "08222222222");
+            
+        List<Customer> customers = customerService.getAllCustomers();
+        System.out.println("All Customers : ");
+        for(int i=0; i < customers.size(); i++) {
+            Customer customer = customers.get(i);
+            System.out.println(i+1 + " - " + customer.getFullName() + " - " + customer.getEmail() + " - " + customer.getPhoneNumber());
+        }   
+
+        System.out.println("\nCustomer Detail : ");
+        System.out.println(customerService.getCustomerById(1L).getDisplayName());
+        
+    }
+}
