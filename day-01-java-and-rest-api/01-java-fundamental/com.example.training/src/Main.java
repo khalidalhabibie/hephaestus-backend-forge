18a200a6759a6b1179bd7942efeb049d510ff9a5
Richo Setiawan <richosetiawan37@gmail.com>
2026-06-11 16:33:30 +0700

    Exercise/Richo-Day01

---
+++ day-01-java-and-rest-api/01-java-fundamental/com.example.training/src/Main.java
@@ -0,0 +1,25 @@
+import model.Customer;
+import service.CustomerService;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerService customerService = new CustomerService();
+
+        customerService.createCustomer("Budi Santoso", "budi@mail.com", "08123456789");
+        customerService.createCustomer("Siti Aminah", "siti@mail.com", "08222222222");
+        customerService.createCustomer("", "siti@mail.com", "08222222222");
+
+        customerService.getAllCustomers();
+
+        Customer customerInput = customerService.getCustomerById(1L);
+
+        System.out.println("Customer Detail:");
+        if (customerInput != null) {
+            System.out.println(customerInput.getDisplayName());
+        }
+
+        customerService.updateCustomerEmail(1L, "thor@gmail.com");
+
+        customerService.deleteCustomer(2L);
+    }
+}
