313e0108f551953f8a028ed57800f95124afa9bb
rasjidzz <mrisjads@gmail.com>
2026-06-11 15:44:52 +0700

    Update : Latihan Soal

--- day-01-java-and-rest-api/01-java-fundamental/com.example.training/src/App.java
+++ day-01-java-and-rest-api/01-java-fundamental/com.example.training/src/App.java
@@ -11,12 +11,13 @@ public class App {
         customerService.createCustomer("Siti Aminah", "siti@mail.com", "08222222222");
 
         customers = customerService.getAllCustomer();
+        int i = 1;
         for (Customer customer : customers) {
-            int i = 1;
             System.out.println(i + " - " + customer.getFullName() + " - " + customer.getEmail() + " - "
                     + customer.getPhoneNumber());
+            i++;
         }
-        Customer cust1 = customerService.getCustomerById(2L);
+        Customer cust1 = customerService.getCustomerById(1L);
         System.out.println("Customer Detail : ");
         cust1.getDisplayName();
     }
