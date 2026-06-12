ab90b3a2f712c9003780aad5e2ab0a4dc09c5132
adnanamiruddin <adnan.amiruddin34@gmail.com>
2026-06-12 17:48:44 +0700

    Complete day 02 exercise

---
+++ day-01-java-and-rest-api/02-springboot-rest-api/exercisespring/src/main/java/com/adnan/exercisespring/dto/CustomerResponse.java
@@ -0,0 +1,29 @@
+package com.adnan.exercisespring.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerResponse {
+    private Long id;
+
+    @JsonProperty("full_name")
+    private String fullName;
+
+    private String email;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    public CustomerResponse(String fullName, String email, String phoneNumber) {
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+}
