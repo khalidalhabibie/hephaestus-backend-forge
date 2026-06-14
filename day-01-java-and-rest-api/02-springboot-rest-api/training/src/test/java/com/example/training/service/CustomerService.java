cb2c9964f56731811400da01651a2bb2018d545d
z <moonsballs@gmail.com>
2026-06-14 21:54:50 +0700

    Exercise/day-02-zeanneta-kayla-azzahra

---
+++ day-01-java-and-rest-api/02-springboot-rest-api/training/src/test/java/com/example/training/service/CustomerService.java
@@ -0,0 +1,54 @@
+package com.example.training.service;
+
+import java.util.*;
+import org.springframework.stereotype.Service;
+import com.example.training.dto.CreateCustomerRequest;
+import com.example.training.dto.CustomerResponse;
+import com.example.training.model.Customer;
+
+@Service
+public class CustomerService {
+
+    private final Map<Long, Customer> db = new HashMap<>();
+    private Long sequence = 1L;
+
+    public CustomerResponse createCustomer(CreateCustomerRequest request) {
+        Customer customer = new Customer(
+                sequence,
+                request.getFullName(),
+                request.getEmail(),
+                request.getPhoneNumber()
+        );
+
+        db.put(sequence, customer);
+        sequence++;
+        return toResponse(customer);
+    }
+
+    public CustomerResponse getCustomerById(Long id) {
+        Customer customer = db.get(id);
+        if (customer == null) return null;
+        return toResponse(customer);
+    }
+
+    public List<CustomerResponse> getCustomers() {
+        List<CustomerResponse> result = new ArrayList<>();
+        for (Customer c : db.values()) {
+            result.add(toResponse(c));
+        }
+        return result;
+    }
+
+    private CustomerResponse toResponse(Customer c) {
+        return new CustomerResponse(
+                c.getId(),
+                c.getFullName(),
+                c.getEmail(),
+                c.getPhoneNumber()
+        );
+    }
+    
+    public void deleteCustomer(Long id) {
+    db.remove(id);
+    }
+}
