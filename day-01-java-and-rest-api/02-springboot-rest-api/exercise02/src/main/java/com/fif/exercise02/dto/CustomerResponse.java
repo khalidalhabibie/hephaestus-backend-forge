6004139011cab9284216d911fdef8a924bb462e4
Wick3dz <dhimaswibowo9999@gmail.com>
2026-06-18 08:46:42 +0700

    "menambahkan json property"

--- day-01-java-and-rest-api/02-springboot-rest-api/exercise02/src/main/java/com/fif/exercise02/dto/CustomerResponse.java
+++ day-01-java-and-rest-api/02-springboot-rest-api/exercise02/src/main/java/com/fif/exercise02/dto/CustomerResponse.java
@@ -8,12 +8,14 @@ import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import lombok.Getter;
 
 @Setter
 @Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonPropertyOrder({"id", "fullName", "email", "phoneNumber", "createdAt", "updatedAt"})
 public class CustomerResponse {
     private Long id;
     // @JsonProperty("full_name")
