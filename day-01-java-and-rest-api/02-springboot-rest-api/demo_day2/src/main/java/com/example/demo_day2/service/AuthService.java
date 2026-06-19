9842f8ab76e48fd6d5f2e655ecf28bace669a4cd
Rivaldy Valentino <rivaldyvalentino100@gmail.com>
2026-06-19 14:12:27 +0700

    Fixing answer Optional Challange Exercise day 5 Rivaldy Valentino

--- day-01-java-and-rest-api/02-springboot-rest-api/demo_day2/src/main/java/com/example/demo_day2/service/AuthService.java
+++ day-01-java-and-rest-api/02-springboot-rest-api/demo_day2/src/main/java/com/example/demo_day2/service/AuthService.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.example.demo_day2.model.Role;
 import com.example.demo_day2.model.User;
 
@@ -12,17 +12,18 @@ import com.example.demo_day2.model.User;
 public class AuthService {
 
     private final Map<String, User> users = new HashMap<>();
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public AuthService() {
-        users.put("admin", new User("admin", "admin123", Role.ADMIN, "token-admin"));
-        users.put("staff", new User("staff", "staff123", Role.STAFF, "token-staff"));
-        users.put("approver", new User("approver", "approver123", Role.APPROVER, "token-approver"));
+        users.put("admin", new User("admin", encoder.encode("admin123"), Role.ADMIN, "token-admin"));
+        users.put("staff", new User("staff", encoder.encode("staff123"), Role.STAFF, "token-staff"));
+        users.put("approver", new User("approver", encoder.encode("approver123"), Role.APPROVER, "token-approver"));
     }
 
     public User login(String username, String password) {
         User user = users.get(username);
 
-        if (user == null || !user.getPassword().equals(password)) {
+        if (user == null || !encoder.matches(password, user.getPassword())) {
             return null;
         }
         return user;
