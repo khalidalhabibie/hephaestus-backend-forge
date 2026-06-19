8b4968dd58ee7f7679d38c52d1f018bd60f4b386
Chris <cristofelnatanael@gmail.com>
2026-06-19 16:52:04 +0700

    Merge branch 'master' of https://github.com/chrstfll/hephaestus-backend-forge into exercise/day-06-christofel

---
+++ day-05-database-sql-jpa-hibernate-flyway/demoSpringbootDatabase/src/test/java/com/example/demoSpringbootDatabase/DemoSpringbootDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoSpringbootDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
